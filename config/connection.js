2fbdc859331818c86ff0fa049b1cb808fbb4e705
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-05-02 21:17:55 -0500

    added files, changed ser.js and connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,15 @@
-//required packages
-const express = require("express");
-const exphbs = require("express-handlebars");
-const path = require("path");
-const session = require('express-session');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-//setting up port & express
-const app = express();
-const PORT = process.env.PORT || 3001;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
-//calls handlebars and sequelize connects to the db
-const hbs = exphbs.create({});
-const sequelize = require("./config/connection");
-
-app.engine("handlebars", hbs.engine)
-app.set("view engine", "handelbars");
-
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers'));
-
-//starts the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+module.exports = sequelize;
