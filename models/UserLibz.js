1717f260022d108d3d2de48c3880e1e6554d44aa
Ryan <pinkstonr54@gmail.com>
2023-05-04 20:49:00 -0500

    added UserLibz.js model

---
+++ models/UserLibz.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserLibz extends Model {}
+
+UserLibz.init(
+    {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.BLOB,
+        allowNull: false,
+    },
+    lib_creater: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_cred_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'userlibz',
+    }
+);
+  
+  module.exports = UserLibz;
