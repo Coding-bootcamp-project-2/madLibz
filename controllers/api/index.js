0d5edd014a67c3dcb33e6a7b458f3b161be8398c
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-05-08 15:46:42 -0500

    initial test

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+const userRoutes = require('./user-routes');
+
+router.use('/users', userRoutes);
 //THESE ARE EXAMPLES
 
 // const categoryRoutes = require('./category-routes');
