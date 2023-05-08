f92da67d0aa0eda2f5af8a4ae7b54fddfd77b954
cantavexp <gunnadar@gmail.comgit config --global user.email gunnadar@gmail.com>
2023-05-08 16:38:37 -0500

    gflie

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
+const madlibzRoutes = require('./Madlibz-routes');
 
-//THESE ARE EXAMPLES
 
-// const categoryRoutes = require('./category-routes');
-// const productRoutes = require('./product-routes');
-// const tagRoutes = require('./tag-routes');
 
-//THESE ARE EXAMPLES
 
-// router.use('/categories', categoryRoutes);
-// router.use('/products', productRoutes);
-// router.use('/tags', tagRoutes);
+router.use('/Madlibz', madlibzRoutes);
+
+
+
+
+
 
 module.exports = router;
