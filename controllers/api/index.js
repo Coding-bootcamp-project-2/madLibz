051ce584e082b34be989f4ebd9aac0efe333f767
Ryan <pinkstonr54@gmail.com>
2023-05-09 13:55:31 -0500

    added userlibz-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
+
 const madlibzRoutes = require('./madlibz-routes');
-const usercredsRoutes = require('./usercreds-routes')
+const usercredsRoutes = require('./usercreds-routes');
+const userlibzRoutes = require("./userlibz-routes")
 
 
 router.use('/Usercreds', usercredsRoutes);
-router.use('/Madlibz', madlibzRoutes)
-
-
-
-
-
+router.use('/Madlibz', madlibzRoutes);
+router.use('/Userlibz', userlibzRoutes);
 
 
 module.exports = router;
