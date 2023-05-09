ed439c9da0201b4cff8ef950d4a44bee544ef9fa
Pinky <pinkstonr54@gmail.com>
2023-05-09 13:14:25 -0500

    changed template data

--- seeds/seedMadlibz.js
+++ seeds/seedMadlibz.js
@@ -3,16 +3,19 @@ const { Madlibz } = require('../models/');
 
 const madlibzData = [
   {
-    title: 'example title 01',
-    content: "example content 01",
+    title: 'Clockwork Orange Lib 01',
+    content: "'Not having that we're not, brotherth. Don't [verb] the [adjective] [noun]!'",
   },
   {
-    title: "example title 02",
-    content: "example content 02"
+    title: "Clockwork Orange Lib 02",
+    content: "I do not wish to describe, brothers, what other horrible [noun] I was like forced to [verb] that [adjective] afternoon."
+  },
+  {
+    title: "Clockwork Orange Lib 03",
+    content: "... and I could [verb] all the [adjective] [noun] sitting down slooshying."
   }
 ];
 
 const seedMadlibz = () => Madlibz.bulkCreate(madlibzData);
 
 module.exports = seedMadlibz;
-
