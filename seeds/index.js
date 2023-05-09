79f6e49254ec0c1b835bb3fa2e49163565f16b8e
michellesambuu <michelle.sambuu@gmail.com>
2023-05-08 19:44:14 -0500

    added seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,2 +1,16 @@
 const sequelize = require('../config/connection');
+const seedMadlibz = require("./seedMadlibz");
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedMadlibz();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
+  
+    process.exit(0);
+  };
+  
+  seedAll();
+
+  
 
