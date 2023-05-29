9250383be292a45d3d6c2be7200d11ca39e796ef
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-05-29 18:15:25 -0500

    post request works and tested

--- controllers/api/madlibz-routes.js
+++ controllers/api/madlibz-routes.js
@@ -6,102 +6,33 @@ const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min)
-}
+};
 
-
-//const { UserLibz } = require('../../models/UserLibz');
 
 router.get("/", (req, res) => {
   try {
     const rando = getRandomInt(0, 4);
     console.log("rando", rando)
     
-    // sequelize.query(`SELECT content FROM madlibz WHERE id='${rando}'`)
+  
     Madlibz.findByPk(rando).then(results => {
-        res.status(200).json(results)
+        res.status(200).json(results);
       })
   } catch (err) {
     res.status(500).json(err)
   }
 });
 
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const dbMadLibzData = await Madlibz.findAll({
-//       include: [
-//         {
-//           model: UserLibz,
-//           attributes: ['title', 'content'],
-//         },
-//       ],
-//     });
-
-//     const madlibz = dbMadLibzData.map((Madlibz) =>
-//       Madlibz.get({ plain: true })
-//     );
-   
-//     res.render('homepage', {
-//       madlibz,
-//       loggedIn: req.session.loggedIn,
-//     });
+router.post("/", async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    const newMadlibz = await Madlibz.create({ title, content });
+    res.status(201).json(newMadlibz);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-
-// router.get('/madlibz/:id', async (req, res) => {
-//   try {
-//     const dbMadlibzData = await Madlibz.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: UserLibz,
-//           attributes: [
-//             'id',
-//             'title',
-//             'content',
-//             'lib_creater',
-//             ],
-//         },
-//       ],
-//     });
-
-//     const madlibz = dbMadlibzData.get({ plain: true });
-   
-//     res.render('madlibz', { madlibz, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-
-// router.get('/userlibz/:id', async (req, res) => {
-//   try {
-//     const dbUserLibzData = await Painting.findByPk(req.params.id);
-
-//     const userlibz = dbUserLibzData.get({ plain: true });
-    
-//     res.render('userlibz', { userlibz, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-
-// router.get('/login', (req, res) => {
-  
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-  
-//   res.render('login');
-// });
 
 module.exports = router;
